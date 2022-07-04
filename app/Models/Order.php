9335198640f4362f53dd86cb9c69a5740fde4c53
MINHTU3107\tumin <tu.minhnguyen3107@gmail.com>
2022-07-05 00:36:17 +0700

    finished UI checkout

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $table = 'orders';
+    protected $fillable = [
+        'customer_name',
+        'phone_number',
+        'address',
+        'city',
+        'country',
+        'discount_code',
+        'discount_price',
+        'payment_method',
+        'total_price',
+        'status',
+        'customer_id',
+        'admin_id',
+    ];
 }
